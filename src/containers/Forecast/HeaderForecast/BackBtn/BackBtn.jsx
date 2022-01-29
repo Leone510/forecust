d8e49f73e987b37067b04ad789f50e5cc80b295a
Leone510 <meteorologsio@gmail.com>
2022-01-29 20:40:13 +0200

    fix link

--- src/containers/Forecast/HeaderForecast/BackBtn/BackBtn.jsx
+++ src/containers/Forecast/HeaderForecast/BackBtn/BackBtn.jsx
@@ -3,12 +3,17 @@ import { StyledButton } from "../../../../components/Button/StyledButton";
 
 export const BackBtn = () => {
 
+   const goToCutaway = () => {
+      window.location.href = "https://leone510.github.io/cutaway/"
+   }
+
    return (
       <StyledButton 
          width={"40px"}
          height={"40px"}
          margin={"5px"}
          color={"#B03F35"}
+         onClick={goToCutaway}
       >
 
          <ImExit size="3em" color="MidnightBlue"/>
